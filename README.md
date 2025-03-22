a8958f6bdc605c31dbe3137c78d83e468495796b
kirstinlyon <kirstin@mltwelve.com>
2025-03-22 21:11:16 +0100

    initial script downloading a random indicator from WHO

---
+++ README.md
@@ -0,0 +1,9 @@
+#WHO Data Dash
+
+WHO Data Dash is an automated solution that connects to the World Health Organization (WHO) API, fetches data at midnight, and pushes it to a data visualization dashboard. This project leverages GitHub Actions for automation, ensuring that your data is always up-to-date.
+
+##Features
+
+-   **Automated Data Fetching**: Randomly fetches an indicator from WHO each midnight.
+
+-   **Data Visualization Integration**: Seamlessly push fetched data to your visualization tool.
---
+++ Scripts/fetch_who_indicator.R
@@ -0,0 +1,114 @@
+# PROJECT: WHO Daily Dash
+# DESCRIPTION: This script fetches the latest data from the WHO API for the
+# AUTHOR: Kirstin Lyon
+# LICENSE: MIT
+# CREATED: 2025-03-22
+
+
+
+# LOAD LIBRARIES -------------------------------------------------------------
+library(httr)
+library(jsonlite)
+library(dplyr)
+library(janitor)
+library(tidyr)
+library(purrr)
+library(glue)
+library(readr)
+
+
+# GLOBAL VARIABLES -----------------------------------------------------------
+URL_BASE <-  "https://ghoapi.azureedge.net/api/"
+
+
+# FUNCTIONS -----------------------------------------------------------------
+#' convert JSON to tibble
+#'
+#' @param url URL of the JSON file
+#'
+#' @returns a tibble
+#' @export
+#'
+#' @examples
+#'  \dontrun{
+#'    convert_JSON_to_tbl(url)
+#' }
+convert_JSON_to_tbl <- function(url){
+    data <- GET(url)
+    data_df <- fromJSON(content(data, as = "text", encoding = "utf-8"))
+    data_tbl <-  map_if(data_df, is.data.frame, list) |> 
+        as_tibble |> 
+        unnest(cols = c(value)) |> 
+        select(-'@odata.context')
+}
+
+
+#' Find an indicator at random that has data
+#'
+#' @param all_indicators a tibble of all indicators
+#' @param URL_BASE the base URL for the WHO API
+#'
+#' @returns a tibble of the indicator data
+#' @export
+#'
+#' @examples
+#'  \dontrun{
+#'    find_indicator(all_indicators, URL_BASE)
+#' }
+
+find_indicator <- function(all_indicators, URL_BASE) {
+    indicator_data_tbl <- tibble()  # Initialize as an empty tibble
+    
+    while (nrow(indicator_data_tbl) == 0) {
+        # Select a random indicator
+        random_indicator <- all_indicators |>
+            sample_n(1) |>
+            select(IndicatorCode) |>
+            pull()
+        
+        # Fetch data for the selected indicator
+        response <- GET(paste0(URL_BASE, random_indicator))
+        indicator_data <- content(response, "text")
+        
+        # Process the fetched data
+        indicator_data_tbl <- fromJSON(indicator_data, simplifyVector = TRUE) |>
+            as_tibble() |>
+            unnest(cols = everything())
+    }
+    
+    return(indicator_data_tbl)
+}
+
+# GET DATA ------------------------------------------------------------------
+
+all_dimension <-  convert_JSON_to_tbl("https://ghoapi.azureedge.net/api/Dimension") 
+all_spatial <-  convert_JSON_to_tbl(paste0(URL_BASE,"Dimension/COUNTRY/DimensionValues")) 
+all_indicators <-  convert_JSON_to_tbl(paste0(URL_BASE,"Indicator")) 
+data <- find_indicator(all_indicators, URL_BASE)
+
+# CLEAN DATA ----------------------------------------------------------------
+
+all_countries <- all_spatial |> 
+    select(Code, Title) |> 
+    rename(spatial_dim = Code, country = Title)
+
+all_regions <- all_spatial |> 
+    select(ParentCode, ParentTitle) |> 
+    rename(parent_location_code = ParentCode, region = ParentTitle) |> 
+    distinct() |> 
+    filter(!is.na(parent_location_code))
+
+data_clean <- data |> 
+    clean_names() |> 
+    select(-c(id, parent_location, comments,
+                   value,date, time_dimension_begin, time_dimension_end)) |> 
+    left_join(all_countries, by = "spatial_dim") |> 
+    left_join(all_regions, by = "parent_location_code") |> 
+    left_join(all_indicators, by = c("indicator_code" = "IndicatorCode") ) |> 
+    left_join(all_dimension, by = c("dim1type" = "Code")) 
+
+# SAVE DATA LOCALLY ------------------------------------------------------
+indicator_id <- unique(data$IndicatorCode)
+write_csv(data_clean, glue("Dataout/who_data_{indicator_id}.csv"))
+
+
